d0bfdc38ac2c32e29eacbd13ed23629357f6434d
Gregory Green <gregoryg@vmware.com>
2022-12-15 07:58:40 -0500

    Adding manual 2 pool manually

--- applications/multi-cluster-demo-app/src/main/java/com/vmare/gemfire/multi/cluster/demo/GemFireConfig.java
+++ applications/multi-cluster-demo-app/src/main/java/com/vmare/gemfire/multi/cluster/demo/GemFireConfig.java
@@ -6,6 +6,7 @@ import com.vmare.gemfire.multi.cluster.demo.domain.Member;
 import org.apache.geode.cache.DataPolicy;
 import org.apache.geode.cache.GemFireCache;
 import org.apache.geode.cache.client.Pool;
+import org.apache.geode.cache.client.PoolManager;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -18,13 +19,21 @@ import org.springframework.data.gemfire.config.annotation.EnablePools;
 @Configuration
 @ClientCacheApplication
 @EnablePools(pools = {
-        @EnablePool(name = "GemFireOne"),
-        @EnablePool(name = "GemFireTwo")
+        @EnablePool(name = "GemFireOne")
         })
 public class GemFireConfig {
 
     @Value("${spring.data.gemfire.pool.default.locators}")
     private String locators;
+
+    @Value("${spring.data.gemfire.pool.gemfiretwo.locators.host}")
+    private String locator2PoolHost;
+
+    @Value("${spring.data.gemfire.pool.gemfiretwo.locators.name}")
+    private String locator2PoolName;
+
+    @Value("${spring.data.gemfire.pool.gemfiretwo.locators.port}")
+    private int locator2PoolPort;
 
     @Bean
     ClientRegionFactoryBean<String, Claim> claimRegion(GemFireCache gemFireCache,
@@ -39,14 +48,17 @@ public class GemFireConfig {
     }
 
     @Bean
-    ClientRegionFactoryBean<String, Member> memberRegion(GemFireCache gemFireCache,
-                                                         @Qualifier("GemFireTwo") Pool gemfireTwoPool)
+    ClientRegionFactoryBean<String, Member> memberRegion(GemFireCache gemFireCache)
     {
         var region = new ClientRegionFactoryBean();
         region.setCache(gemFireCache);
         region.setDataPolicy(DataPolicy.EMPTY);
         region.setName("Member");
-        region.setPool(gemfireTwoPool);
+
+        var p2 = PoolManager.createFactory().addLocator(locator2PoolHost,locator2PoolPort).create(locator2PoolName);
+
+        region.setPool(p2);
+
         return region;
     }
 }
